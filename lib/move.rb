331141b81ff31e9729133097c59c98a36bbff7ca
Taylor Mills <taylormills24@yahoo.com>
2019-02-20 17:34:25 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,10 +11,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(input)
-  index = input.to_i - 1
+def move(index, user_marker = 'X')
+    @board[index] = user_marker
+  end
 end
-
-def move(board, index, user_marker = "X")
-  board[index.to_i] = user_marker
-end 
